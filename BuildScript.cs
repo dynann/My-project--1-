72c66fc5d64fd4850a674ff999f9addf49b8ac9d
dynann <dynannran@gmail.com>
2026-05-26 22:42:51 +0700

    add BuildScript for iOS build process

---
+++ BuildScript.cs
@@ -0,0 +1,40 @@
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System.IO;
+
+public class BuildScript
+{
+    public static void BuildiOS()
+    {
+        string buildPath = "Builds/iOS";
+
+        if (!Directory.Exists(buildPath))
+        {
+            Directory.CreateDirectory(buildPath);
+        }
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = GetScenes(),
+            locationPathName = buildPath,
+            target = BuildTarget.iOS,
+            options = BuildOptions.None
+        };
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result != BuildResult.Succeeded)
+        {
+            throw new System.Exception("Build failed");
+        }
+    }
+
+    private static string[] GetScenes()
+    {
+        return new string[]
+        {
+            "Assets/Scenes/SampleScene.unity"
+        };
+    }
+}
